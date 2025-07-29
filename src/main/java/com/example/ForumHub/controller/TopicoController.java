fc7a39dd450314acfbfd37911add95e5177d3589
Felipe <felipe.matheussms@gmail.com>
2025-07-29 17:57:37 -0300

    listar e detalhar

--- src/main/java/com/example/ForumHub/controller/TopicoController.java
+++ src/main/java/com/example/ForumHub/controller/TopicoController.java
@@ -2,19 +2,23 @@ package com.example.ForumHub.controller;
 
 import com.example.ForumHub.dto.DadosCadastroTopico;
 import com.example.ForumHub.dto.DadosDetalhamentoTopico;
+import com.example.ForumHub.repository.TopicoRepository;
 import com.example.ForumHub.service.TopicoService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/topicos")
 public class TopicoController {
+
+    @Autowired
+    private TopicoRepository topicoRepository;
 
     @Autowired
     private TopicoService topicoService;
@@ -28,4 +32,28 @@ public class TopicoController {
 
         return ResponseEntity.ok(dadosDetalhamento);
     }
+
+    @GetMapping
+    public ResponseEntity<Page<DadosDetalhamentoTopico>> listar(@PageableDefault(size = 10, sort = {"dataCriacao"}) Pageable paginacao) {
+        var pageDeTopicos = topicoRepository.findAll(paginacao);
+
+        var pageDeTopicosDTO = pageDeTopicos.map(DadosDetalhamentoTopico::new);
+
+        return ResponseEntity.ok(pageDeTopicosDTO);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DadosDetalhamentoTopico> detalhar(@PathVariable Long id) {
+        var topico = topicoRepository.findById(id);
+
+        if (topico.isPresent()) {
+            var dadosDetalhamento = new DadosDetalhamentoTopico(topico.get());
+            return ResponseEntity.ok(dadosDetalhamento);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+
 }
+
